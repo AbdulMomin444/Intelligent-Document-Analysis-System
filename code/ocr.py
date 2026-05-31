77f079e1052093e1ed74fd3f2e82e3f967e8aea4
AbdulMomin444 <111608857+AbdulMomin444@users.noreply.github.com>
2026-05-31 23:26:05 +0500

    Create ocr.py

---
+++ code/ocr.py
@@ -0,0 +1,14 @@
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def extract_text(image):
+
+    custom_config = r'--oem 3 --psm 6'
+
+    text = pytesseract.image_to_string(
+        image,
+        config=custom_config
+    )
+
+    return text
